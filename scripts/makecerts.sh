813c97efcf54f45624fff917332ac9e193780d97
Ryan Bastic <ryan.bastic@booking.com>
2014-08-03 20:41:56 +0200

    makecerts mods so we have keypairs for everything

--- scripts/makecerts.sh
+++ scripts/makecerts.sh
@@ -4,8 +4,17 @@
 # ./makecert.sh foo@foo.com
 mkdir certs
 rm certs/*
-echo "make server cert"
+echo "make boogie server cert"
 openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Example Company/OU=IT/CN=www.example.com/emailAddress=$1"
-echo "make client cert"
+echo "make boogie client cert"
 openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Example Company/OU=IT/CN=www.example.com/emailAddress=$1"
 
+echo "make bagent server cert"
+openssl req -new -nodes -x509 -out certs/bagent-server.pem -keyout certs/bagent-server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Example Company/OU=IT/CN=www.example.com/emailAddress=$2"
+echo "make bagent client cert"
+openssl req -new -nodes -x509 -out certs/bagent-client.pem -keyout certs/bagent-client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Example Company/OU=IT/CN=www.example.com/emailAddress=$2"
+
+echo "make bcli server cert"
+openssl req -new -nodes -x509 -out certs/bcli-server.pem -keyout certs/bcli-server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Example Company/OU=IT/CN=www.example.com/emailAddress=$3"
+echo "make bcli client cert"
+openssl req -new -nodes -x509 -out certs/bcli-client.pem -keyout certs/bcli-client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Example Company/OU=IT/CN=www.example.com/emailAddress=$3"
