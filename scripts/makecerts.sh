9d392cd2cd09b210b19056beecc19b8165f029b9
Ryan Bastic <ryan.bastic@booking.com>
2014-08-03 14:16:33 +0200

    Sample keys and cert generation tool for TLS support.

---
+++ scripts/makecerts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# call this script with an email address (valid or not).
+# like:
+# ./makecert.sh foo@foo.com
+mkdir certs
+rm certs/*
+echo "make server cert"
+openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Example Company/OU=IT/CN=www.example.com/emailAddress=$1"
+echo "make client cert"
+openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Example Company/OU=IT/CN=www.example.com/emailAddress=$1"
+
